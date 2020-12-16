cba3162d8406d17c1ae64e5eb653e401c8f82e58
adamdelancey <adamdelancey@gmail.com>
2020-12-16 15:00:44 +0000

    create add_task template

--- app.py
+++ app.py
@@ -104,6 +104,11 @@ def logout():
     return redirect(url_for("tips"))
 
 
+@app.route("/add_tip")
+def add_tip():
+    return render_template("add_tip.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
