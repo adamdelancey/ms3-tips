529b39f21163808e04a09f60afd9f0103b192c3d
adamdelancey <adamdelancey@gmail.com>
2020-12-16 15:24:37 +0000

    add task form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,12 @@
   $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
+    $('.datepicker').datepicker({
+        format: "dd mmm yyyy",
+        yearRange: 0,
+        showClearBtn: true,
+        i18n:{
+            done: "Select"
+        }
+    });
   });
         
